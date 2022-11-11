668ae5a4f6d5da43fd7e7668e4baeec216fd557c
yamakasivan <yamakasivan@mail.ru>
2022-11-11 09:58:43 +0800

    step

---
+++ Курс/Модуль 7. Верстка страниц/7.6. Создание форм/task1/anketa.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>anketa</title>
+</head>
+
+<body>
+    <?php
+    echo $_POST['lastname'];
+    // echo $_POST['firstname'];
+    // echo $_POST['otch'];
+    // echo $_POST['nikname'];
+    // echo $_POST['birthday'];
+    // echo $_POST['howold'];
+    // echo $_POST['tel'];
+    // echo $_POST['email'];
+    // echo $_POST['url'];
+    // echo $_POST['profile'];
+    // echo $_POST['month'];
+    // echo $_POST['color'];
+    // echo $_POST['pet'];
+    // echo $_POST['eat'];
+    // echo $_POST['comment']; 
+
+    ?>
+</body>
+
+</html>
