684efe2a78bdaac291335e4739e8552aa4cbedeb
yamakasivan <yamakasivan@mail.ru>
2022-11-11 12:05:01 +0800

    step

---
+++ Курс/Модуль 8. Программирование на стороне клиента и на стороне сервера/8.1.5 Подключение, переменные, условные операторы/js/index.js
@@ -0,0 +1,69 @@
+// console.log("Тест подключения JS через файл");
+// console.info("test info")
+// console.info("debug info")
+// console.info("error info")
+// console.info("alarm info")
+
+// console.error("test error")
+
+// console.warn("attention")
+
+
+// let num;
+// num = 5;
+// console.log("Значение переменной " + num);
+
+// let num = 7;
+// console.log("Значение переменной " + num);
+
+
+// let num;
+// num = 5;
+// num = 7;
+// console.log("Значение переменной " + num);
+
+
+// let num;
+// num = 5;
+// num = 7;
+
+// const number = 10;
+
+// let str = "Строка";
+// let num_1 = 3.1;
+// let boolevo = true;
+
+// str = 19;
+
+
+// let x = 5, y = 17;
+// let z;
+
+// z = x + y;
+// z = y - 5;
+// z = y - x;
+// z = x * y;
+// z = y / x;
+// z = y % x;
+
+// x = x + 20;
+// x += 20;
+
+// x = x + 1;
+// x++;
+// x--;
+
+
+// z = Math.PI;
+// z = Math.sin(90);
+// z = Math.cos(90);
+
+// z = Math.max(1, 4, 6, 78, 45, 0);
+// z = Math.min(1, 4, 6, 78, 45, 0, -1);
+
+
+let x = 5, y = "5";
+y = Number(y);
+z = x + y;
+
+console.log("Число " + z);
