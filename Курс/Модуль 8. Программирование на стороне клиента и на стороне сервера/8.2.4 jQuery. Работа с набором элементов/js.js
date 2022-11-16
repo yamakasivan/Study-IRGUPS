c678fc35a1486a01429a0c5621aa5ec47a91ae7d
yamakasivan <yamakasivan@mail.ru>
2022-11-16 15:43:23 +0800

    step

---
+++ Курс/Модуль 8. Программирование на стороне клиента и на стороне сервера/8.2.4 jQuery. Работа с набором элементов/js.js
@@ -0,0 +1,83 @@
+// $(document).ready(function () {
+
+    // $(".btn").click(function () {
+
+
+        // // Задачи на работу с набором
+        // // 1
+        // $("li").not(".test");
+
+        // // 2
+        // $("li").first();
+
+        // // 3
+        // $("li").last();
+
+        // // 4
+        // $("li").eq(4);
+
+        // // 5
+        // $("li").last().prev();
+
+        // // 6
+        // $("li").slice(2, 10);
+
+        // // 7
+        // $("li").filter(".www");
+
+
+        // // Задачи на соседей
+        // // 1
+        // $(".test").next();
+
+        // // 2
+        // $(".test").prev();
+
+        // // 3
+        // $(".test").nextAll();
+
+        // // 4
+        // $(".test").prevAll();
+
+        // // 5
+        // $(".www").nextUntil(".test");
+
+        // // 6
+        // $(".test").prevUntil(".www");
+
+        // // 7
+        // $("li.www").siblings();
+
+        // // 8
+        // $("li.www").siblings(".test");
+
+        // // 9
+        // $("li.www").siblings(":not(.test)");
+        // // ИЛИ
+        // $("li.www").siblings().not(".test");
+
+
+        // // Задачи на родителей
+        // // 1
+        // $(".test").parent();
+
+        // // 2
+        // $("b").parent().css("color", "red");
+
+        // // 3
+        // $("b").closest("div").css("color", "red");
+
+
+        // // Задачи на find, add, andSelf, end
+        // // 1
+        // $("h2.www").css("color", "red").find(".test").css("fontSize", "30px");
+
+        // // 2
+        // $(".www").css("color", "red").find("p").append("!");
+
+        // // 3
+        // $("p.www").prepend("!").append("!!").add("h2").css("color", "red");
+
+
+    // });
+// });
